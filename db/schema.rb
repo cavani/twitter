689a56ab50d90ac32a3fdad9b149388f7f28a751
cavani <afcavani@gmail.com>
2011-04-22 14:42:17 -0300

    user model with passwords, and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418195050) do
+ActiveRecord::Schema.define(:version => 20110420165242) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
